9006f6b4f08667e1293fd370dd797a0af626c5d5
虎落平阳变傻屌 <3039829493@qq.com>
2017-03-19 23:42:44 +0800

    update user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index() 
     {
         return view('user');
@@ -14,5 +19,10 @@ class UserController extends Controller
     public function respwd()
     {
         
+    }
+    
+    public function profiles()
+    {
+    
     }
 }

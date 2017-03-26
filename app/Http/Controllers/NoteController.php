b745659c02bb3eb96c31b37eed00f5984ef8b084
LawnTiger <3039829493@qq.com>
2017-03-26 23:48:03 +0800

    add note controller

---
+++ app/Http/Controllers/NoteController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NoteController extends Controller
+{
+    public function index()
+    {
+        return 'note index';
+    }
+
+    public function create()
+    {
+    
+    }
+
+    public function destroy()
+    {
+    
+    }
+}
+

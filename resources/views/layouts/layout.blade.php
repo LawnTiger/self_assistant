fb5f78e06ef22186c08c6354be724a89b61bac1b
余灿豪 <3039829493@qq.com>
2017-03-27 17:41:31 +0800

    View add layout and errors

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>assistant - @yield('title')</title>
+    @yield('style')
+    @yield('script')
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,37 +1,27 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>user</title>
-</head>
-<body>
-    <h3>user arear</h3>
-    TODO
-    <h4>reset password</h4>
-    <div>
-        <form action="{{ action('UserController@resetpwd') }}" method="POST">
-            old word:<input type="password" name="old_word" /><br />
-            new word:<input type="password" name="new_word" /><br />
-            {{ csrf_field() }}
-            <input type="submit" />
-            <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-            </ul>
-        </form>
-    </div>
-    <h4>update profile</h4>
-    <div>
-        <form action="{{ action('UserController@profiles') }}" method="POST">
-            username:<input type="text" name="name" /><br />
-            {{ csrf_field() }}
-            <input type="submit" />
-            <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-            </ul>
-        </form>
-    </div>
-</body>
-</html>
+@extends('layouts.layout')
+
+@section('title', 'user')
+
+@section('content')
+<h3>user arear</h3>
+TODO
+<h4>reset password</h4>
+<div>
+    <form action="{{ action('UserController@resetpwd') }}" method="POST">
+        old word:<input type="password" name="old_word" /><br />
+        new word:<input type="password" name="new_word" /><br />
+        {{ csrf_field() }}
+        <input type="submit" />
+    </form>
+</div>
+<h4>update profile</h4>
+<div>
+    <form action="{{ action('UserController@profiles') }}" method="POST">
+        username:<input type="text" name="name" /><br />
+        {{ csrf_field() }}
+        <input type="submit" />
+    </form>
+</div>
+@include('layouts.errors')
+
+@endsection

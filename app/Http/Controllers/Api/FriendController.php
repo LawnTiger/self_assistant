cbaa2390c16541df1308a8dddcca0e5f89080e51
豪 <3039829493@qq.com>
2017-10-24 10:35:13 +0800

    add friends

--- app/Http/Controllers/Api/FriendController.php
+++ app/Http/Controllers/Api/FriendController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Friend;
+use App\Models\User;
 
 class FriendController extends Controller
 {
@@ -15,30 +16,35 @@ class FriendController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $user_id = $request->user()->id;
+        $add_id = User::whereEmail($request->email)->value('id');
+
+        if (!empty($add_id)) {
+            $add = Friend::isAdd($user_id, $add_id);
+            if (empty($add) || $add->status == 2) {
+                Friend::addFriend($user_id, $add_id);
+                return app('jResponse')->success(['add_id' => $add_id]);
+            } elseif ($add->status == 0) {
+                return app('jResponse')->success(['add_id' => $add_id]);
+            } else {
+                return app('jResponse')->error('该用户已添加');
+            }
+        } else {
+            return app('jResponse')->error('未找到用户');
+        }
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -49,7 +55,7 @@ class FriendController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -60,8 +66,8 @@ class FriendController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -72,7 +78,7 @@ class FriendController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)

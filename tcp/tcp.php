25586f4d7838615968f515995b2ed0b81b886359
余灿豪 <3039829493@qq.com>
2017-10-31 14:24:47 +0800

    tcp: save socket connect to db

--- tcp/tcp.php
+++ tcp/tcp.php
@@ -11,12 +11,12 @@ $channel = new Channel\Server('127.0.0.1', 2206);
 
 $tcp = new Worker("tcp://0.0.0.0:4000");
 $tcp->count = 1;
-$mapping = [];
 
 // tcp
 $tcp->onWorkerStart = function ($tcp) {
     Channel\Client::connect('127.0.0.1', 2206);
-    global $mapping;
+    global $db;
+    $db = new Workerman\MySQL\Connection();
 
     Channel\Client::on('broadcast', function ($event_data) use ($tcp) {
         foreach ($tcp->connections as $con) {
@@ -24,16 +24,15 @@ $tcp->onWorkerStart = function ($tcp) {
         }
     });
 
-    // 订阅 p2p 事件并注册事件处理函数
-    Channel\Client::on('p2p', function ($event_data) use ($tcp) {
-        $to_connection_id = $event_data['to_connection_id'];
-        $to_worker_id = $event_data['to_worker_id'];
+    // 订阅 p2p-id 事件并注册事件处理函数
+    Channel\Client::on('p2p-' . $tcp->id, function ($event_data) use ($tcp) {
+        $to_connection_id = $event_data['connection'];
         $message = $event_data['content'];
-        if ($to_worker_id != $tcp->id || !isset($tcp->connections[$to_connection_id])) {
-            return;
+        if (!isset($tcp->connections[$to_connection_id])) {
+            echo 'error connect id' . $to_connection_id;
+            return ;
         }
-        $to_connection = $tcp->connections[$to_connection_id];
-        $to_connection->send($message);
+        $tcp->connections[$to_connection_id]->send($message);
     });
 
 
@@ -66,58 +65,38 @@ function handle_connect($connection)
 
 function handle_message($connection, $data)
 {
-    global $mapping;
     global $tcp;
+    global $db;
 
     print("worker: $tcp->id; connection: $connection->id Receive: $data \n");
     $connection->lastMessageTime = time();
 
     $data = json_decode($data, true);
     if ($data['code'] == 'init') {
-        $mapping[] = ['user' => $data['data']['id'], 'worker' => $tcp->id, 'connection' => $connection->id];
+        $db->insert('socket_mapping')->cols(array(
+            'user_id' => $data['data']['id'],
+            'worker' => $tcp->id,
+            'connection' => $connection->id))
+            ->query();
     } elseif ($data['code'] == 'msg') {
         $id = $data['data']['id'];
-        $map = mapping(['id' => $id]);
-        if (! empty($map)) {
-            $to_worker_id = $map['worker'];
-            $to_connection_id = $map['connection'];
-            $content = $data['data']['content']['body'];
-
-            Channel\Client::publish('p2p', array(
-                'to_worker_id'     => $to_worker_id,
-                'to_connection_id' => $to_connection_id,
-                'content'          => $content
+        $map = $db->select('*')->from('socket_mapping')->where("user_id=$id")->row();var_dump($map);
+        if (!empty($map)) {
+            Channel\Client::publish('p2p-' . $map['worker'], array(
+                'connection' => $map['connection'],
+                'content' => $data['data']['content']['body']
             ));
         }
     }
-    print_r($mapping);
 }
 
 function handle_close($connection)
 {
-    global $mapping;
     global $tcp;
+    global $db;
+    $db->delete('socket_mapping')->where("connection=$connection->id and worker=$tcp->id")->query();
 
     echo "Connection : {$connection->id}  closed\n";
-
-    foreach ($mapping as $key => $item) {
-        if ($item['worker'] == $tcp->id && $item['connection'] == $connection->id) {
-            unset($mapping[$key]);
-        }
-    }
-    print_r($mapping);
-}
-
-function mapping($select)
-{
-    global $mapping;
-    if (array_has($select, 'id')) {
-        foreach ($mapping as $item) {
-            if ($item['user'] == $select['id']) {
-                return $item;
-            }
-        }
-    }
 }
 
 // Run worker

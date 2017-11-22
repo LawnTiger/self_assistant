98dfd18a1caa207fb373311dc261d06af62f40a3
余灿豪 <3039829493@qq.com>
2017-11-22 18:42:01 +0800

    fix socket msg overflow

--- database/migrations/2017_11_11_141432_create_socket_msg.php
+++ database/migrations/2017_11_11_141432_create_socket_msg.php
@@ -11,7 +11,7 @@ class CreateSocketMsg extends Migration
         Schema::create('socket_msg', function (Blueprint $table) {
             $table->bigInteger('user_id');
             $table->tinyInteger('type')->default(0)->comment('0-未读, 1-已读');
-            $table->string('msg');
+            $table->text('msg');
         });
     }
 
